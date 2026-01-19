58b04e757d1ed176853029c027e081c0a0dc40e8
ilyass bourass <ilyass.b.1992@gmail.com>
2026-01-19 20:58:13 +0100

    feat : ajout des routes pour le dashboard, les produits et l from d'ajout d'un produit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,23 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login';
 import {MainLayoutComponent} from './layout/main-layout/main-layout';
 import {DashboardComponent} from './pages/dashboard/dashboard';
+import {ProductsComponent} from './pages/products/products';
+import {ProductForm} from './pages/products/product-form/product-form';
+import {ProductDetails} from './pages/products/product-details/product-details';
+import {RegisterComponent} from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  {path : 'register' ,component : RegisterComponent},
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: 'dashboard', component: DashboardComponent }
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'products/form', component: ProductForm },
+      { path: 'products/details/:id', component: ProductDetails }
     ]
   }
 ];
