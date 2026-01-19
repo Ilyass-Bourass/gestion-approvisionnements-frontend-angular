38b9a788e665f237eb29d12f8a56de9f6293a2ca
ilyass bourass <ilyass.b.1992@gmail.com>
2026-01-19 20:56:37 +0100

    config : configuration des interceptors HTTP (JWT et erreurs) dans app.config

--- src/app/core/interceptors/jwt-interceptor.ts
+++ src/app/core/interceptors/jwt-interceptor.ts
@@ -1,5 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+
+  if (token) {
+
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(authReq);
+  }
+
   return next(req);
 };
